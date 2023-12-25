f6fba5d7d04af6588a4a2f0b2467478a25cd3a2c
GloryKO <koladeg2022@gmail.com>
2023-12-25 19:53:30 +0100

    added custom permissions

--- blog_api/settings.py
+++ blog_api/settings.py
@@ -38,14 +38,21 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'accounts.apps.AccountsConfig',
     'posts.apps.PostsConfig',
 ]
 REST_FRAMEWORK={
     "DEFAULT_PERMISSION_CLASSES":[
-        "rest_framework.permissions.AllowAny",
+        "rest_framework.permissions.IsAuthenticated",
     ]
 }
+# new
+CORS_ORIGIN_WHITELIST = (
+"http://localhost:3000",
+"http://localhost:8000",
+)
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- posts/views.py
+++ posts/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 from rest_framework import generics 
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
 
 class PostListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly)
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+    
 
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     
+
